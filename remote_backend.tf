cf0a0d7287692adc5d92f0f558901166d5de119c
Torsten Stueber <torsten.stueber@db.com>
2021-05-18 13:19:25 +0000

    Added the first label

--- remote_backend.tf
+++ remote_backend.tf
@@ -4,7 +4,7 @@
 
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "torsten176"
     workspaces {
       name = "hashicat-gcp"
